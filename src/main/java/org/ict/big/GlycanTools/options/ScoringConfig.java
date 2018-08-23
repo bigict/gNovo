0d67cdac9e8606ecd3831d4ab2e45b4b2e1a9c95
Sunmile <wanghuiL55@126.com>
2018-08-23 17:03:48 +0800

    create gNovo project

---
+++ src/main/java/org/ict/big/GlycanTools/options/ScoringConfig.java
@@ -0,0 +1,66 @@
+package org.ict.big.GlycanTools.options;
+
+import org.eurocarbdb.application.glycanbuilder.FragmentOptions;
+import org.ict.big.GlycanTools.io.SpectrumFileType;
+
+public class ScoringConfig {
+
+    /**
+     * The type of the input spectrum file.
+     */
+    public SpectrumFileType spectrumFileType = SpectrumFileType.mzXML;
+
+    /**
+     * The resolution of this experiment.
+     */
+    public double resolution = 250.0;
+    /**
+     * The options of fragmenter.
+     */
+    public GlycanFragmentOptions fragmentOptions = new GlycanFragmentOptions();
+
+    /**
+     * The m/z tolerance of the matched theoretical peak and experimental peak.
+     */
+    public double annotationTolerance = 0.5;
+
+    public double moleculeMassTolerance = 2.0;
+    public double precursorMassTolerance = 0.3;
+
+    /**
+     * starting from MS0.
+     */
+    public double[] weightForEachStage = {0, 0, 0.25, 0.25, 0.25, 0.25};
+
+    public double averageWeight = 0.25;
+
+    public double isotopicTolerence = 0.1;
+
+    public double minMz = 200.0;
+
+    public double defaultFilterRatio = 0.01;
+
+    public double minFilterRatio = 0.02;
+
+    public double minIntsAllowedOfIsoDistribution = 1;
+
+    public int minNumberOfIsotopicPeakGroupsMoreThan1500 = 2;
+
+    public int minNumberOfIsotopicPeakGroupsLessThan1500 = 0;
+
+    public double baselineMin = 2.0 + 0.001;
+
+    public int minNumberOfPeaks = 2;
+
+    public double alphaForTanh = 0.2;
+
+    public boolean scoreOnlyMs2 = false;
+
+    public boolean sigmoidNormalize = true;
+
+    public ScoringConfig(){
+        fragmentOptions.MAX_NO_CLEAVAGES = 3;
+        fragmentOptions.max_no_cleavages_for_very_stage = 6;
+        fragmentOptions.no_cleavages_added_at_each_stage = 3;
+    }
+}
